b216057d7c5fb9ff53737db249bbd074ffa47010
Carey Costa <careyxcosta@gmail.com>
2017-01-16 15:03:57 -0500

    exercise3 age complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
-puts "What is your name?"
-name = gets.chomp
-puts "Hi, nice to meet you #{name}!"
+#puts "What is your name?"
+#name = gets.chomp
+#puts "Hi, nice to meet you #{name}!"
+
+puts "How old are you?"
+age = gets.chomp
+puts "Wow, #{age} is pretty old"
